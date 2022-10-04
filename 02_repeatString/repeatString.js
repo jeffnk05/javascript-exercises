e4ec8546994ff9b4e5cda871a9229b80c798fb89
Jeff <j.edjekoomhene@gmail.com>
2022-10-04 08:48:17 +0200

    Solved exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
 const repeatString = function() {
-
+    if(num < 0) {
+        return "ERROR"
+    }
+    let word = ""
+    for(let i = 0; i < num; i++){
+        word += string
+    }
+    return word
 };
 
 // Do not edit below this line
